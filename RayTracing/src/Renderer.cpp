2ab76c2562f594bee24ff46c7e05a2fb69f0b9af
Andre Mauricio Zelak <andre.zelak@gmail.com>
2023-04-25 23:29:40 -0300

    Fix image aspect ratio
    Cumpute the viewport aspect ratio and apply it
    to the coordinate.
    
    Signed-off-by: Andre Mauricio Zelak <andre.zelak@gmail.com>

--- RayTracing/src/Renderer.cpp
+++ RayTracing/src/Renderer.cpp
@@ -28,7 +28,9 @@ void Renderer::Render()
 		for (uint32_t x = 0; x < m_FinalImage->GetWidth(); x++)
 		{
 			glm::vec2 coord = { (float)x / (float)m_FinalImage->GetWidth(), (float)y / (float)m_FinalImage->GetHeight() };
-			coord = coord * 2.0f - 1.0f;
+			coord = coord * 2.0f - 1.0f; // (-1,-1) to (1,1) coordinate space
+			float aspectRatio = (float)m_FinalImage->GetWidth() / (float)m_FinalImage->GetHeight();
+			coord.x = coord.x * aspectRatio;
 			m_ImageData[x + y * m_FinalImage->GetWidth()] = PerPixel(coord);
 		}
 	}
@@ -39,8 +41,8 @@ void Renderer::Render()
 uint32_t Renderer::PerPixel(glm::vec2 coord)
 {
 	glm::vec3 rayOrigin(0.0f, 0.0f, 2.0f);
-	// rayDirection = glm::normalize(rayDirection);
 	glm::vec3 rayDirection(coord.x, coord.y, -1.0f);
+	//rayDirection = glm::normalize(rayDirection);
 	glm::vec3 circleOrigin(0.0f, 0.0f, -2.0f);
 	float radius = 0.5f;
 
@@ -57,7 +59,12 @@ uint32_t Renderer::PerPixel(glm::vec2 coord)
 	float c = glm::dot(rayOrigin, rayOrigin) - 2.0f * glm::dot(rayOrigin, circleOrigin) - radius * radius;
 
 	// quadratic formula discriminant = b^2 - 4ac
-	float discriminant = b * b - 4 * a * c;
+	float discriminant = b * b - 4.0f * a * c;
+
+	// quadratic formula
+	// t = (-b +- sqr(discriminant))/2a
+
+	
 
 	uint32_t color = 0xff000000;
 	if (discriminant >= 0)
